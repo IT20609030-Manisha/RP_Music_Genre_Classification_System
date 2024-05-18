22d4776d30edf7be6ff7f8031798d345471661ee
IT20609030-Manisha <98085146+IT20609030-Manisha@users.noreply.github.com>
2024-05-18 08:10:00 +0530

    Display Trending tracks

--- frontend/src/components/DisplayGenre.js
+++ frontend/src/components/DisplayGenre.js
@@ -6,6 +6,8 @@ const DisplayGenre = ({ predictedGenre }) => {
     const CLIENT_ID = "041e658da36f4c6383218a56609423fa";
     const CLIENT_SECRET = "5673f05eebdd4169a8d54fba0f6f1e13";
     const [accessToken, setAccessToken] = useState("");
+    const [isFetching, setIsFetching] = useState(false);
+    const [trendingTracks, setTrendingTracks] = useState([]);
 
     useEffect( () => {
         //API Access Token
@@ -22,6 +24,34 @@ const DisplayGenre = ({ predictedGenre }) => {
         .then(data => setAccessToken(data.access_token))
     }, [])
 
+    const fetchTrendingTracks = async () => {
+        setIsFetching(true);
+        // const token = 'YOUR_SPOTIFY_ACCESS_TOKEN';  // Replace with your Spotify access token
+        const url = `https://api.spotify.com/v1/recommendations?seed_genres=${predictedGenre.toLowerCase()}`;
+
+        try {
+            const response = await fetch(url, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer '+accessToken
+                }
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setTrendingTracks(data.tracks);
+                console.log(data)
+            } else {
+                console.error('Failed to fetch recommendations');
+            }
+        } catch (error) {
+            console.error('Error fetching recommendations:', error);
+        } finally {
+            setIsFetching(false);
+        }
+    };
+
     return (
         <div className="genre-bars mt-3">
             <div className="genre-bar">
@@ -29,37 +59,25 @@ const DisplayGenre = ({ predictedGenre }) => {
                 <h2 className="label">Genre : {predictedGenre}</h2>
             </div>
             <div className="mt-3">
-                <button type="button" className="btn btn-outline-primary">
-                    View More Info <i className="fa-solid fa-circle-info"></i>
+                <button type="button" className="btn btn-outline-primary" onClick={fetchTrendingTracks}>
+                    View Trending Tracks <i className="fa-solid fa-circle-info"></i>
                 </button>
             </div>
-            <div class="card" style={{ width: '18rem', marginBottom: '1rem' }}>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
+            <div className="trending-tracks mt-3">
+                {trendingTracks.length > 0 && trendingTracks.map(track => (
+                    <div className="card" key={track.id}>
+                        <img src={track.album.images[0]?.url} className="card-img-top" alt={`${track.album.name} cover`} />
+                        <div className="card-body">
+                            <h5 className="card-title">{track.name}</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">Artist: {track.artists[0].name}</h6>
+                            <p className="card-text">Album: {track.album.name} <br/> Release Date: {track.album.release_date}</p>
+                            <a href={track.external_urls.spotify} className="card-link" target="_blank" rel="noopener noreferrer">Link to Song</a>
+                            <a href={track.album.external_urls.spotify} className="card-link" target="_blank" rel="noopener noreferrer">Link to Album</a>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div class="card" style={{ width: '18rem', marginBottom: '1rem' }}>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
-            </div>
-            <div class="card" style={{ width: '18rem', marginBottom: '1rem' }}>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
-            </div>
+            
         </div>
     )
 }
